a97e2fb162ed8fe44d4a0d6ce0bccba0d0115ca7
DA2574540 <syafi.id.self@gmail.com>
2025-01-20 20:20:08 +0700

    first commit

---
+++ JS/SideBar.js
@@ -0,0 +1,9 @@
+        // Fungsi untuk membuka atau menutup sidebar
+        function toggleSidebar() {
+            document.body.classList.toggle('sidebar-open');
+        }
+
+        // Fungsi untuk menutup sidebar
+        function closeSidebar() {
+            document.body.classList.remove('sidebar-open');
+        }
